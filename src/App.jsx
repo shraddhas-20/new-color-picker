8a95783f7a51144379ad42e94ec61d41b799b033
Shraddha Singh <shraddhaforcode@gmail.com>
2024-12-22 13:35:31 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ColorPicker from './ColorPicker.jsx';
+import DigitalClock from './DigitalClock.jsx';
+import ToDoList from './ToDoList.jsx';
+
+function App() {
+
+    return (
+    <>
+<ColorPicker/>
+</>
+   
+  );
+}
+
+export default App

95283c4404a3197d73ba277ec953ff4888b4cb29
shraddha-20 <shraddhaforcode@gmail.com>
2024-12-22 11:12:01 +0530

    color picker

---
+++ new-react-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Counter from './Counter.jsx'
+import MyComponent from './MyComponent.jsx';
+import ColorPicker from './ColorPicker.jsx';
+
+function App() {
+
+  return (
+    <>
+<ColorPicker/>
+</>
+   
+  );
+}
+
+export default App
